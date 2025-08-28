52649fb4d078f0ed7da367902fa7ec62011d828a
Manu <emmanuellymadeira@gmailcom>
2025-08-27 21:33:50 -0300

    Fiz a atividade
    Montei o projeto e fiz a atividade

---
+++ AtividadeClassHeroM3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Peça ao usuário que informe se está chovendo (responda 'sim' ou 'não'). 
+//Se estiver chovendo, informe que deve levar guarda-chuva; 
+//caso contrário, informe que não precisa.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Esta chovendo Hoje?");
+        Console.WriteLine("Se sim, digite s");
+        Console.WriteLine("Se nao, digite n");
+        
+        string resposta = Console.ReadLine();
+
+        if (resposta == "s")
+        {
+            Console.WriteLine("Melhor Levar um guarda-chuva!");
+        }
+        else if (resposta == "n")
+        {
+            Console.WriteLine("Curta o dia, deixe o guarda-chuva em casa");
+        }else
+        {
+            Console.WriteLine("Desculpe, nao entendi =(");
+        }
+    }
+}
